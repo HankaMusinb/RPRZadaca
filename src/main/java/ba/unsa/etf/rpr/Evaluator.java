9cbce5efdde57a8d5403dd857b5d49ea9cc4b3ce
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2022-11-19 22:20:20 +0100

    Dodani testovi i nepostojece klase

--- src/main/java/ba/unsa/etf/rpr/Evaluator.java
+++ src/main/java/ba/unsa/etf/rpr/Evaluator.java
@@ -9,36 +9,46 @@ public class Evaluator {
     public Evaluator(String ulaz) {
         this.ulaz = ulaz;
     }
-    public void ispravnostUlaza(String ulaz){
+    public void ispravnostUlaza(String ulaz) throws RuntimeException{
         if(!provjeriRedoslijed(ulaz)){
-            //throw RuntimeException;
+            throw new RuntimeException("Pogresan unos.");
         }else if(!provjeriBrojZagrada(ulaz)){
-            // throw RuntimeException;
+             throw new RuntimeException("Pogresan unos.");
         }else if(!provjeriIspravnostKaraktera(ulaz)){
-            // throw RuntimeException;
-        }else if(!provjeri(ulaz)){
-            //  throw RuntimeException;
+            throw new RuntimeException("Pogresan unos.");
         }
     }
+
+    /**
+     * Konvertiranje ulaza u listu stringova razdvojenih razmacima
+     */
     private String[] konvertuj(String ulaz){
+
         return ulaz.split(" ");
     }
     private boolean provjeriIspravnostKaraktera(String ulaz) {
         for(String karakter : konvertuj(ulaz)){
-            if(dozvoljeniKarakteri(karakter) == false){
+            if(!dozvoljeniKarakteri(karakter)){
                 return false;
             }
         }
         return true;
     }
+
+    /**
+     * provjera da li se u unesenom stringu nalaze zagrade, brojevi i operandi
+     * @param s
+     * @return
+     */
     private boolean dozvoljeniKarakteri(String s){
         if( s.equals("(") || s.equals(")") || provjeriJeLiOperacija(s) || provjeriJeLiBroj(s)){
             return true;
         }
         return false;
     }
+
     private boolean provjeriJeLiOperacija(String s){
-        if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")){
+        if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("sqrt")){
             return true;
         }
         return false;
@@ -58,16 +68,33 @@ public class Evaluator {
     private boolean provjeriParnostZagrada(String[] lista){
         Integer br1 = 0, br2 = 0;
         for(String s : lista){
-            if (s == "(") br1 = br1 + 1;
-            if (s == ")") br2 = br2 + 1;
+            if (s.equals("(")) br1 = br1 + 1;
+            if (s.equals( ")")) br2 = br2 + 1;
         }
         if (!br1.equals(br2)) return false;
         return true;
     }
-    private boolean provjeriRedoslijed(String ulaz) {
-        return true;
-    }
-    private boolean provjeri(String ulaz) {
+    private boolean provjeriRedoslijed(String ulaz) throws RuntimeException {
+        String[] lista = konvertuj(ulaz);
+        int i, brOtvorenihZagrada = 0;
+        for(i = 0; i < lista.length; i++){
+            if(lista[i].equals("(")){
+                brOtvorenihZagrada = brOtvorenihZagrada + 1;
+                int j = i;
+                while(j < lista.length){
+                    if ( lista[j].equals(")")){
+                        brOtvorenihZagrada = brOtvorenihZagrada - 1;
+                        break;
+                    }
+                    if( lista[j].equals("(")){
+                        brOtvorenihZagrada = brOtvorenihZagrada + 1;
+                    }
+                    j++;
+                }
+            }
+
+        }
+        if(brOtvorenihZagrada == 0) return true;
         return false;
     }
 
--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
@@ -1,7 +1,38 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ExpressionEvaulatorTest {
     /**
      * minimalno pet unit testova
      */
+    @Test
+    public void ispitajIzraz(){
+        Double d = -9.0;
+        assertEquals(d, new ExpressionEvaluator().evaluate("( ( 2 - 3 ) * ( 4 + 5 ) )"));
+    }
+    @Test
+    public void ispitajIzrazSaKorijenom(){
+        assertEquals(3, new ExpressionEvaluator().evaluate("( 1 + sqrt ( ( 1 + 3 ) ) )"));
+    }
+    @Test
+    public void nepravilanUnosSaRazmacima() throws RuntimeException {
+        assertThrows(RuntimeException.class, () ->{
+            new ExpressionEvaluator().evaluate("( 1 +2 )");
+        });
+    }
+    @Test
+    public void nepravilanUnosSaSlovima() throws RuntimeException {
+        assertThrows(RuntimeException.class, () ->{
+            new ExpressionEvaluator().evaluate("( 1 +7k )");
+        });
+    }
+    @Test
+    public void nepravilanUnosSaOperandima() throws RuntimeException {
+        assertThrows(RuntimeException.class, () ->{
+            new ExpressionEvaluator().evaluate("( 1 +*2 )");
+        });
+    }
+
 }
